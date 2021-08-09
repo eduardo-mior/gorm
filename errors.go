587904367269fd1eff51fcb6a8a861a34bc4b824
miorsbsistemas <mior@sbsistemas.com.br>
2021-08-09 18:02:59 -0300

    fix: fix incorrect message no records in result set

--- errors.go
+++ errors.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// ErrNoRecordsInResultSetSQL sql native error on querying with .row() function or similar
-	ErrNoRecordsInResultSetSQL = errors.New("sql: no rows in the result set")
+	ErrNoRecordsInResultSetSQL = errors.New("sql: no rows in result set")
 	// ErrRecordNotFound returns a "record not found error". Occurs only when attempting to query the database with a struct; querying with a slice won't return this error
 	ErrRecordNotFound = errors.New("record not found")
 	// ErrInvalidSQL occurs when you attempt a query with invalid SQL
